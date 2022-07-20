b6c307cb4c81fb739819dfb9e152543691ba9345
Lucas <86057306+LcsZro@users.noreply.github.com>
2022-07-20 12:29:21 -0300

    att

---
+++ ex003/ex002.lua
@@ -0,0 +1,13 @@
+function aula6()
+
+print('Digite um numero')
+numero1 = io.read()
+print('Digite um segundo numero')
+numero2 = io.read()
+print(numero1 > numero2)
+
+    if (numero1 > numero2) then 
+        print('O primeiro numero é maior que o segundo')
+    end
+
+end
---
+++ ex003/ex003.lua
@@ -0,0 +1,17 @@
+function aula6()
+
+    print('Digite um numero')
+    numero1 = io.read()
+    print('Digite um segundo numero')
+    numero2 = io.read()
+    print(numero1 > numero2)
+    
+        if (numero1 > numero2) then 
+            print('O primeiro numero é maior que o segundo')
+        elseif (numero1 < numero2) then
+            print('O segundo numero é maior que o primeiro')
+        else 
+            print('Os numeros são iguais')
+        end
+    
+    end
